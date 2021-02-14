8f0f0de471c5f5501be27e9dd8d92c88c09489ca
SchlenkR <ronald.schlenker@binarygears.de>
2021-02-14 11:06:39 +0100

    cvbcvb

--- test.fsx
+++ test.fsx
@@ -6,11 +6,11 @@ type Expr =
     | Int of int
     | Float of float
     | String of string
-    | Var of ident:string
-    | Let of ident:string * assignment:Expr * body:Expr
-    | Fun of ident:string * body:Expr
-    | App of target:Expr * arg:Expr
-    // TODO: Bool,Ctor,If
+    | Var of ident: string
+    | Let of ident: string * assignment: Expr * body: Expr
+    | Fun of ident: string * body: Expr
+    | App of target: Expr * arg: Expr
+// TODO: Bool,Ctor,If
 
 type Typ =
     | TypInt
@@ -23,20 +23,23 @@ type TExpr =
     | TFloat of TypExpr<float>
     | TString of TypExpr<string>
     | TVar of TypExpr<{| ident: string |}>
-    | TLet of TypExpr<{| ident:string; assignment:TExpr; body:TExpr |}>
-    | TFun of TypExpr<{| ident:string; body:TExpr |}>
-    | TApp of TypExpr<{| target:TExpr; arg:TExpr |}>
-and TypExpr<'a> = { typeInfo: TypeInfo; value: 'a }
-and TypeInfo =
-    | Typed of Typ
-    | Untyped of typeVarName: string
+    | TLet of
+        TypExpr<{| ident: string
+                   assignment: TExpr
+                   body: TExpr |}>
+    | TFun of TypExpr<{| ident: string; body: TExpr |}>
+    | TApp of TypExpr<{| target: TExpr; arg: TExpr |}>
 
-let typExpr (typeInfo, value) = { typeInfo = typeInfo; value = value }
-    
+and TypExpr<'a> = { typeInfo: TypInfo; value: 'a }
+and TypInfo =
+    | Det of Typ
+    | Open of TypVar
+and TypVar = TypVar of int
+
 // module Print =
 //     let printTExpr (expr: TExpr) =
 //         match expr with
-//         | TInt x -> 
+//         | TInt x ->
 //         | TFloat x
 //         | TString x
 //         | TVar of ident: string
@@ -46,49 +49,63 @@ let typExpr (typeInfo, value) = { typeInfo = typeInfo; value = value }
 
 
 module Infer =
+    
+    // type Constraint =
+    //     { termA:  }
+    
     module private Map =
-        let set key value map = map |> Map.change key (fun _ -> Some value)
+        let set key value map =
+            map |> Map.change key (fun _ -> Some value)
 
-    let toTypableAst (identMap: Map<string, TypeInfo>) (expr: Expr) =
+    let toTypableAst (identMap: Map<string, TypInfo>) (expr: Expr) =
         let mutable typeCounter = -1
-        let makeUntyped() =
+
+        let newTypeVar () =
             typeCounter <- typeCounter + 1
-            Untyped $"t_{typeCounter}"
-        let rec gen (identMap: Map<string, TypeInfo>) (expr: Expr) =
-            match expr with
-            | Int x ->
-                TInt (typExpr (Typed TypInt, x))
-            | Float x ->
-                TFloat (typExpr (Typed TypFloat, x))
-            | String x ->
-                TString (typExpr (Typed TypString, x))
-            | Var ident ->
-                TVar (typExpr (makeUntyped(), {| ident = ident |}))
+            Open(TypVar typeCounter)
+
+        let typExprVar typeVar value = { typeInfo = typeVar; value = value }
+
+        let typExpr value =
+            { typeInfo = newTypeVar ()
+              value = value }
+
+        let rec gen (identMap: Map<string, TypInfo>) = function
+            | Int x -> TInt(typExpr x)
+            | Float x -> TFloat(typExpr x)
+            | String x -> TString(typExpr x)
+            | Var ident -> TVar(typExpr {| ident = ident |})
             | Let (ident, assignment, body) ->
-                let identMap = identMap |> Map.set ident (makeUntyped())
-                TLet (
-                    typExpr (
-                        makeUntyped(),
+                let identMap =
+                    identMap |> Map.set ident (newTypeVar ())
+                TLet
+                    (typExpr
                         {| ident = ident
                            assignment = gen identMap assignment
-                           body = gen identMap body |}))
+                           body = gen identMap body |})
             | Fun (ident, body) ->
-                let identMap = identMap |> Map.set ident (makeUntyped())
-                TFun (
-                    typExpr (
-                        makeUntyped(),
+                let identMap =
+                    identMap |> Map.set ident (newTypeVar ())
+                TFun
+                    (typExpr
                         {| ident = ident
-                           body = gen identMap body |}))
+                           body = gen identMap body |})
             | App (target, arg) ->
-                TApp (
-                    typExpr (
-                        makeUntyped(),
+                TApp
+                    (typExpr
                         {| target = gen identMap target
-                           arg = gen identMap arg |}))
-        
+                           arg = gen identMap arg |})
+
         gen identMap expr
 
-
+    let genConstraintSet = function
+        | TInt x -> ()
+        | TFloat x -> ()
+        | TString x -> ()
+        | TVar x -> ()
+        | TLet x -> ()
+        | TFun x -> ()
+        | TApp x -> ()        
 
 
 
@@ -96,25 +113,24 @@ module Infer =
 
 let expr1 = Int 42
 
-let expr2 = Let ("hurz", Int 43, Int 32)
+let expr2 = Let("hurz", Int 43, Int 32)
 
 let add =
-    let addA = Fun("a", App(Var "libcall_add", Var "a"))
+    let addA =
+        Fun("a", App(Var "libcall_add", Var "a"))
+
     Fun("b", App(addA, Var "b"))
 
 let expr3 =
-    Let ("hurz", Int 43,
-        Let ("f", add,
-            App (App (Var "f", Var "hurz"), Int 99)))
+    Let("hurz", Int 43, Let("f", add, App(App(Var "f", Var "hurz"), Int 99)))
 
 
 
 
 let test expr =
     let lib =
-        [
-            "libcall_add", Typed (TypFun (TypInt, TypFun (TypInt, TypInt)))
-        ]
+        [ "libcall_add", Det(TypFun(TypInt, TypFun(TypInt, TypInt))) ]
+
     Infer.toTypableAst (lib |> Map.ofList) expr
 
 test expr3
