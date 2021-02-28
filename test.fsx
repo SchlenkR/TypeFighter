7d4d9af8b401a18b6704db166acd9087e5d6c5c6
SchlenkR <ronald.schlenker@binarygears.de>
2021-02-28 16:28:50 +0100

    geg

--- test.fsx
+++ test.fsx
@@ -2,42 +2,44 @@
 fsi.PrintWidth <- 250
 #endif
 
-type Lit =
-    | LInt of int
+type Lit
+    = LInt of int
     | LFloat of float
     | LString of string
-type Exp =
-    | ELit of Lit
+type Exp
+    = ELit of Lit
     | EVar of string
     | EApp of Exp * Exp
     | EFun of string * Exp
     | ELet of string * Exp * Exp
 
-type MType =
-    | MInt
+type ErrorMsg = string
+
+type MType
+    = MInt
     | MFloat
     | MString
     | MFun of TVar * TVar
-and TExp =
-    | TLit of Lit
+and TVar
+    = Free of int
+    | Det of MType
+    | Unresolvable of ErrorMsg
+and TExp
+    = TLit of Lit
     | TVar of ident: string
     | TLet of {| ident: string; assignment: TExpAnno; body: TExpAnno |}
     | TFun of {| ident: Ident; body: TExpAnno |}
     | TApp of {| target: TExpAnno; arg: TExpAnno |}
 and Ident = { name: string; tvar: TVar }
-and Env = Map<string, TVar>
 and TExpAnno = { expr: TExp; annotation: TVar; env: Env }
-and TVar =
-    | Free of int
-    | Det of MType
-    | Unresolvable
+and Env = Map<string, TVar>
 
 
 module Infer =
     
-    type Constraint = { left: TVar;  right: TVar }
+    type Equation = { left: TVar;  right: TVar }
     
-    type ConstraintTableEntry = { desc: string; constr: Constraint }
+    type ConstraintTableEntry = { desc: string; constr: Equation }
     type VarTableEntry = { desc: string; nr: int; value: obj }
 
     type private TVarGen() =
@@ -49,8 +51,8 @@ module Infer =
             do table.Add entry
             Free tyCounter
         member this.newTyExpAnno(desc, value: obj, env, expr) =
-            let tyVar = this.newTVar(desc, value)
-            { expr = expr; annotation = tyVar; env = env }
+            let tvar = this.newTVar(desc, value)
+            { expr = expr; annotation = tvar; env = env }
         member this.Table = table |> Seq.toList
         
     type private ConstrGen() =
@@ -67,7 +69,7 @@ module Infer =
             map |> Map.change ident (fun _ -> Some typAnno)
         let resolveAnnotation (ident: string) (map: Map<string, TVar>) =
             match map |> Map.tryFind ident with
-            | None -> Unresolvable
+            | None -> Unresolvable $"Identifier {ident} is undefined."
             | Some ta -> ta
 
     let annotate (env: Env) (expr: Exp) =
@@ -110,11 +112,11 @@ module Infer =
                 match typExpr.expr with
                 | TLit lit ->
                     match lit with
-                    | LInt _ -> yield cgen.constrain("Int", typExpr.annotation, Det MInt)
-                    | LFloat _ -> yield cgen.constrain("Float", typExpr.annotation, Det MFloat)
-                    | LString _ -> yield cgen.constrain("String", typExpr.annotation, Det MString)
+                    | LInt x -> yield cgen.constrain($"Int {x}", typExpr.annotation, Det MInt)
+                    | LFloat x -> yield cgen.constrain($"Float {x}", typExpr.annotation, Det MFloat)
+                    | LString x -> yield cgen.constrain($"String {x}", typExpr.annotation, Det MString)
                 | TVar var ->
-                    yield cgen.constrain("Var", typExpr.annotation, typExpr.env |> Map.resolveAnnotation var)
+                    yield cgen.constrain($"Var {var}", typExpr.annotation, typExpr.env |> Map.resolveAnnotation var)
                 | TApp appExpr ->
                     // res = add 20 -> we know something about "add":
                     // It is a function that goes from int to whatever 'res' is
@@ -125,29 +127,63 @@ module Infer =
                     yield cgen.constrain("Fun", typExpr.annotation, Det(MFun(funExpr.ident.tvar, funExpr.body.annotation)))
                     yield! genConstraints funExpr.body
                 | TLet letExpr ->
-                    yield cgen.constrain("Let", typExpr.annotation, letExpr.body.annotation)
+                    yield cgen.constrain($"Let {letExpr.ident}", typExpr.annotation, letExpr.body.annotation)
                     yield! genConstraints letExpr.assignment
                     yield! genConstraints letExpr.body
             ]
-        let res = genConstraints typExpr |> Set.ofList
+        let res = genConstraints typExpr
         (res, cgen.Table)
 
     let constrain lib expr =
-        let a,b = annotate lib expr
+        let a,_ = annotate lib expr
         let c,d = genConstraintSet a
         (c,d)
 
-    let solve
+    type TraceResult
+        = Cycle
+        | Resolved of MType
+        | NotFound
+        | Inherited
 
+    type VarTreeNode = { var: int; result: TraceResult; children: VarTreeNode list }
+
+    let rec collectFreeVars (t: TVar) =
+        match t with
+        | Det m ->
+            match m with
+            | MInt | MFloat | MString -> []
+            | MFun (m, n) ->
+                [ yield! collectFreeVars m
+                  yield! collectFreeVars n ]
+        | Free x -> [ x ]
+        | Unresolvable msg -> failwith $"Unresolvable: {msg}"
+        
+    let traceVar (var: int) (equations: Equation list)  =
+        let rec doTraceVar (var: int) (visited: Set<int>) =
+            let findEq i = equations |> List.tryFind (fun eq -> match eq.left with | Free x -> x = i | _ -> false)
+            
+            match Set.contains var visited with
+            | true -> { var = var; result = Cycle; children = [] }
+            | false ->
+                match findEq var with
+                | None -> { var = var; result = NotFound; children = [] }
+                | Some v ->
+                    let newVisited = Set.add var visited
+                    let res =
+                        collectFreeVars v.right
+                        |> List.map (fun v -> doTraceVar v newVisited)
+                    { var = var; result = Inherited; children = res }
+        doTraceVar var Set.empty
+        
 
 module Helper =
     let printVarTable (_, table: Infer.VarTableEntry list) =
-        for e in table do
+        for e in table |> List.sortByDescending (fun e -> e.nr) do
             printfn "%-15s %-5d %A" e.desc e.nr e.value
     
     let printConstrTable (_, table: Infer.ConstraintTableEntry list) =
-        for e in table do
-            printfn "%-10s    %A = %A" e.desc e.constr.left e.constr.right
+        for e in table |> List.sortByDescending (fun e -> e.constr.left) do
+            printfn "%-20s    %A = %A" e.desc e.constr.left e.constr.right
 
 
 
@@ -177,15 +213,18 @@ let expr3 =
 
 expr3 |> Infer.annotate lib |> Helper.printVarTable
 expr3 |> Infer.constrain lib |> Helper.printConstrTable
+expr3 |> Infer.constrain lib |> fst |> Infer.traceVar 3
 
 
-module X =
-    
-    let libcall_add a b = a + b
-    let app f x = f x
-    
-    let hurz = 43
-    let f = fun b -> fun a -> (libcall_add a) b
-    (f hurz) 99
+Infer.collectFreeVars (Det (MFun (Free 13, Free 14)))
 
+// module X =
+//     
+//     let libcall_add a b = a + b
+//     let app f x = f x
+//     
+//     let hurz = 43
+//     let f = fun b -> fun a -> (libcall_add a) b
+//     (f hurz) 99
+//
 
