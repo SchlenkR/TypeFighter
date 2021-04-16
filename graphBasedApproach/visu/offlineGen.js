82b50d1be1eb2727b5286992867ed81d7bf4700a
ronaldschlenker <ronald.schlenker@binarygears.de>
2021-04-16 10:41:24 +0200

    gdfg

--- graphBasedApproach/visu/offlineGen.js
+++ graphBasedApproach/visu/offlineGen.js
@@ -1,37 +1,35 @@
-// This example loads a web page with a GoJS diagram,
-// then creates a screenshot of the Diagram with makeImageData, plus a screenshot of the page.
 
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 
 const parseDataUrl = (dataUrl) => {
     const matches = dataUrl.match(/^data:(.+);base64,(.+)$/);
     if (matches.length !== 3) {
         throw new Error('Could not parse data URL.');
     }
-    return { mime: matches[1], buffer: window.Buffer.from(matches[2], "base64") };
+    return { mime: matches[1], buffer: Buffer.from(matches[2], "base64") };
 };
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    // This does not have to be a page on the web, it can be a localhost page, or file://
-    await page.goto('https://gojs.net/samples/orgChartEditor.html', {
+    const graphUrl = path.join(__dirname, 'web/graph.html');
+    //console.log(graphUrl);
+    await page.goto(`file://${graphUrl}`, {
         waitUntil: "networkidle2" // ensures images are loaded
     });
 
     const imageData = await page.evaluate(() => {
-        window.myDiagram.animationManager.stopAnimation();
-        return window.myDiagram.makeImageData({
-            background: window.myDiagram.div.style.backgroundColor
+        myDiagram.animationManager.stopAnimation();
+        return myDiagram.makeImageData({
+            background: myDiagram.div.style.backgroundColor
         });
     });
 
-    // Output the GoJS makeImageData as a .png:
     const { buffer } = parseDataUrl(imageData);
-    fs.writeFileSync('diagram-screenshot.png', buffer, 'base64');
+    fs.writeFileSync('diagram.png', buffer, 'base64');
 
-    // Output a page screenshot
-    await page.screenshot({ path: 'page-screenshot.png' });
+    //await page.screenshot({ path: 'page-screenshot.png' });
     await browser.close();
 })();
